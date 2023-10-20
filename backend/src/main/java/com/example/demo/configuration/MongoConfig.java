4e0abbbcc33939cfcd627c526516cfbf7b7cad5a
Brad Babines <bbabines@gmail.com>
2023-10-20 09:39:52 -0400

    fix: removed uri from application.properties

--- backend/src/main/java/com/example/demo/configuration/MongoConfig.java
+++ backend/src/main/java/com/example/demo/configuration/MongoConfig.java
@@ -4,12 +4,14 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
+@PropertySource("classpath:secrets.properties")
 public class MongoConfig extends AbstractMongoClientConfiguration {
 
-    @Value("${spring.data.mongodb.uri}")
+   @Value("${mongodb.uri}")
     private String connectionString;
 
     @Value("${spring.data.mongodb.database}")
